769cd4040e4a8af28cdc1850151e418ee49e7ba9
Fehlingerist <ksztnnnn@gmail.com>
2026-02-19 18:17:23 +0100

    Branch - Test AST design direction (idk how to call it yet)

--- build_test.ps1
+++ build_test.ps1
@@ -1,4 +1,4 @@
-g++ -std=c++26 tests/bundle.cpp -I"C:/dev/C_C++/StdToolset/" -I"src" -I"tests" -D_DEBUG -o "test/test.exe" -g
+g++ -std=c++26 tests/bundler.cpp -I"C:/dev/C_C++/StdToolset/" -I"src" -I"tests" -D_DEBUG -o "test/test.exe" -g
 
 if ($LASTEXITCODE -ne 0) {
     Write-Error "Compilation failed (exit code $LASTEXITCODE)"
--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -1,5 +1,31 @@
 #include <lexer/lexer.hpp>
+#include <linear_allocator/linear_allocator.hpp>
 
 namespace ASTParser{
-    
+    class ParserContext{
+        Util::Lexer lexer;
+        Util::LinearAllocator linear_allocator;
+        
+        public:
+        ParserContext(Util::Source& source): lexer(source)
+        {};
+
+        bool generate_AST()
+        {
+            
+        };
+    };
+
+    class Parser{
+        ParserContext parser_context;
+        public:
+
+        Parser(Util::Source& source): parser_context(source)
+        {};        
+
+        bool generate_AST()
+        {
+            return parser_context.generate_AST();
+        };
+    };
 };
